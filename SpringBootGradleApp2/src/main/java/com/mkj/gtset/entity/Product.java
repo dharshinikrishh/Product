aaf8687544041b5a31d4c7a5e15dc2e1cf83b6c2
dharshinikrishh <114347465+dharshinikrishh@users.noreply.github.com>
2022-11-03 20:16:24 +0530

    Add files via upload

---
+++ SpringBootGradleApp2/src/main/java/com/mkj/gtset/entity/Product.java
@@ -0,0 +1,29 @@
+package com.mkj.gtset.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int productCode;
+	private String category;
+	private String productName;
+	private int price ;
+	private String dealerName;
+	private float userRating;	
+	
+}
